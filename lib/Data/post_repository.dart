74b9b810a02e649d631b5230257dabd9a8a101f7
System 16 <mittuparmar19@gmail.com>
2023-11-07 17:20:33 +0530

    Post Repository Done.

--- lib/Data/post_repository.dart
+++ lib/Data/post_repository.dart
@@ -1,5 +1,18 @@
-class PostRepository{
-    void fetchPosts ()async{
-      
+import 'package:dio/dio.dart';
+
+import 'api/api_file.dart';
+import 'my_user_model.dart';
+
+class PostRepository {
+  Api api = Api();
+  Future<List<MyAPI>> fetchPosts() async {
+    try {
+      Response response = await api.sendRequest.get('/posts');
+      List<dynamic> postMaps = response.data;
+      return postMaps.map((postMaps) => MyAPI.fromJson(postMaps)).toList();
+    } catch (ex) {
+      throw Exception();
+      rethrow;
     }
+  }
 }
