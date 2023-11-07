a0d156f5ee16802ba44d08afb95eb706c214dbc4
System 16 <mittuparmar19@gmail.com>
2023-11-07 13:16:00 +0530

    Required Packages are Added,Home page and other, folders added

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent,elevation: 0,title: const Text('API Calling with Bloc'),),
+
+    );
+  }
+}
